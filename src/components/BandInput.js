8dcace04d227357e53844bcf675e53232c71320a
github username <sharnellgrant@gmail.com>
2021-12-23 21:25:01 -0500

    commit tree ee0e551de3807ce05094f691713a237d847bce97

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,47 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  
+    state = {
+      name: ''
+    }
+  
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: '',
+    });
+  }
+
+
   render() {
+     
+
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <p>
+          <input 
+          type="text" 
+          placeholder="add band" 
+          value={this.state.name}
+          onChange={this.handleChange}/> 
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.name}
       </div>
     )
   }
 }
 
 export default BandInput
+//The form goes here. It should have a text field and a submit button.
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput';
+import Bands from '../components/Bands'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <Bands bands={this.props.bands}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (band) => dispatch({ type: "ADD_BAND", payload: band})
+  };
+};
+
+const mapStateToProps = (state) => {
+  // return state
+  return {
+    bands: state.bands
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+
+//conncts to redux store and should be a child of the app component
+//dispatch should be setup so they can be passed down to other components
